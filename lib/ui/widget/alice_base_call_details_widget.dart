e3f0475eadcf2a89384785cbfc4c679786e16b5f
hieubt <hieu.bui@onemount.com>
2022-07-10 20:17:57 +0700

    Change

--- lib/ui/widget/alice_base_call_details_widget.dart
+++ lib/ui/widget/alice_base_call_details_widget.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_alice/helper/alice_conversion_helper.dart';
 import 'package:flutter_alice/ui/utils/alice_parser.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:markdown/markdown.dart' as md;
 
 abstract class AliceBaseCallDetailsWidgetState<T extends StatefulWidget> extends State<T> {
   final JsonEncoder encoder = new JsonEncoder.withIndent('  ');
@@ -11,20 +12,25 @@ abstract class AliceBaseCallDetailsWidgetState<T extends StatefulWidget> extends
   Widget getListRow(String name, String value) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         SelectableText(name, style: TextStyle(fontWeight: FontWeight.bold)),
         Padding(padding: EdgeInsets.only(left: 5)),
-        Flexible(
-            fit: FlexFit.loose,
-            child: MarkdownBody(
-              data: value.isEmpty ? '' : '```\n$value\n```',
-              selectable: true,
-            )
-            // SelectableText(
-            //   value,
-            // ),
+        Expanded(
+          child: MarkdownBody(
+            data: value.isEmpty ? '' : '```json\n$value\n```',
+            selectable: true,
+            softLineBreak: true,
+            styleSheetTheme: MarkdownStyleSheetBaseTheme.material,
+            extensionSet: md.ExtensionSet(
+              md.ExtensionSet.gitHubFlavored.blockSyntaxes,
+              [
+                md.EmojiSyntax(),
+                ...md.ExtensionSet.gitHubFlavored.inlineSyntaxes,
+              ],
             ),
+          ),
+        ),
         Padding(
           padding: EdgeInsets.only(bottom: 18),
         )
